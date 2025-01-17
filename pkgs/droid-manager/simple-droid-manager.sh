19babfaa8e042728e8463737725eb0fd7a567bd3
Waldemar Tomme <waldetomme@gmail.com>
2025-01-17 11:42:01 +0100

    Remove file on script exit

--- pkgs/droid-manager/simple-droid-manager.sh
+++ pkgs/droid-manager/simple-droid-manager.sh
@@ -5,6 +5,7 @@ set -e
 CONFIGURATION=$1
 
 nix bundle --bundler .#androidShell .#droidManagerConfigurations.$CONFIGURATION
+trap "rm droid-manager-generation-android 2>/dev/null || true" EXIT
 adb push droid-manager-generation-android /tmp/droid-manager-activate
 rm droid-manager-generation-android
 adb shell /tmp/droid-manager-activate
