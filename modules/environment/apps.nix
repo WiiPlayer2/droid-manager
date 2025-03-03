8bd3176293b056df39974fb413972eca64e01f85
Waldemar Tomme <waldetomme@gmail.com>
2025-03-03 22:40:23 +0100

    Uninstall apks installed by droid-manager

--- modules/environment/apps.nix
+++ modules/environment/apps.nix
@@ -17,8 +17,34 @@ in
           apksPaths = concatStringsSep " " cfg;
           adb = "${pkgs.android-tools}/bin/adb";
           installCommand = "${adb} install-multi-package -d ${apksPaths}";
+          pnames =
+            concatStringsSep
+            "\n"
+            (
+              sortOn
+              (x: x)
+              (
+                map
+                (x: x.meta.pname)
+                cfg
+              )
+            );
+          pnamesFile = pkgs.writeText "installed-apps" pnames;
+          uninstallCommand = ''
+            INSTALLED_APPS="$(${adb} shell cat /storage/self/primary/.droid-manager/installed_apks)"
+            UNINSTALLABLE_APPS="$(${pkgs.coreutils}/bin/comm -3 <(echo "$INSTALLED_APPS") ${pnamesFile})"
+            for apk in $UNINSTALLABLE_APPS; do
+              # adb uninstall -k $apk
+              adb shell cmd package uninstall -k $apk
+            done
+            adb push ${pnamesFile} /storage/self/primary/.droid-manager/installed_apks
+          '';
+          setupCommand = ''
+            ${uninstallCommand}
+            ${installCommand}
+          '';
         in
-          installCommand;
+          setupCommand;
     };
   };
 }
