3a677903f8e89cb3c888dc6e3ac9c6eb18136aca
Waldemar Tomme <waldetomme@gmail.com>
2025-03-07 20:26:25 +0100

    Add functionType to androidApps types

--- flake-modules/androidApps.nix
+++ flake-modules/androidApps.nix
@@ -3,15 +3,23 @@ let
   inherit (lib)
     mkOption
     types
+    mkOptionType
+    isFunction
+    mergeOneOption
     ;
   inherit (flake-parts-lib)
     mkTransposedPerSystemModule
     ;
+  functionType = mkOptionType {
+    name = "function";
+    check = isFunction;
+    merge = mergeOneOption;
+  };
 in
 mkTransposedPerSystemModule {
   name = "androidApps";
   option = mkOption {
-    type = with types; lazyAttrsOf pathInStore;
+    type = with types; lazyAttrsOf (oneOf [ pathInStore functionType ]);
     default = { };
     description = ''
       An attribute set of Android apps
